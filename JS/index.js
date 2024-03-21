8100a786d55476b24fbc454b7b6ed25953aad56f
tiendung832005 <tiendungx832005@gmail.com>
2024-03-21 08:03:16 +0700

    add file

--- JS/index.js
+++ JS/index.js
@@ -1,111 +1,114 @@
-let productList = [
-    {
-        Id: 1,
-        images: [],
-        name: "MERCEDES C200 AVANTGARDE 2023",
-        engine: "I4",
-        status: true,
-        sold:"8",
-        size: "4751 x 1890 x 1437",
-        wattage: "150kW [204hp] tại 6100 vòng/phút",
-        price: "$90000 | $900/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+// Lấy dữ liệu trên local
+const userLogin = JSON.parse(localStorage.getItem("userLogin")) ;
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-    {
-        Id: 2,
-        images: [],
-        name: "Mercedes GLB 200 AMG 2022",
-        engine: "I4",
-        status: true,
-        sold:"10",
-        size: "4655 mm x 1840 mm x 1680 mm",
-        wattage: "163 hp tại 5.500 vòng/phút",
-        price: "$90000 | $900/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+const userLoginElement = document.getElementById("userLogin");
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-    {
-        Id: 3,
-        images: [],
-        name: "Mercedes C300 AMG 2022",
-        engine: "I4 2.0L",
-        status: true,
-        sold:"15",
-        size: "4688 x 1810 x 1442 (mm)",
-        wattage: "255 Mã Lực",
-        price: "$97000 | $978/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+if(userLogin){
+    // Hiển thị tên user đang đăng nhập lên header
+    userLoginElement.innerHTML = userLogin.userName;
+}else{
+    userLoginElement.innerHTML = ""
+}
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-    {
-        Id: 4,
-        images: [],
-        name: "Mercedes E200 Exclusive 2021",
-        engine: "I4",
-        status: true,
-        sold:"23",
-        size: "4923x1852x1468 (mm)",
-        wattage: "145kW [197hp] tại 5500-6100 vòng/phút",
-        price: "$94000 | $936/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+// Mảng chứa sản phẩm
+// let products = [
+//     {
+//         image: "../asset/image/mercedes-c200-avantgarde-plus-2022-2023-2024-2025-ngoai-that-noi-that-mercedeshaxaco-com-vn-3.jpg",
+//         name: "Mercedes C200 Avantgarde 2023",
+//         price: "$90000 | $900/month",
+//         id: 1,
+//         sold: "2023",
+//     },
+//     {
+//         image: "../asset/image/car2 glb 200 amg.jpg",
+//         name: "Mercedes GLB 200 AMG 2022",
+//         price: "$90000 | $900/month",
+//         id: 2,
+//         sold: "2022",
+//     },
+//     {
+//         image: "../asset/image/car3 c300 amg.jpg",
+//         name: "Mercedes C300 AMG 2021",
+//         price: "$97000 | $978/month",
+//         id: 3,
+//         sold: "2021",
+//     },
+//     {
+//         image: "../asset/image/car4 e200 exclusive.jpg",
+//         name: "Mercedes E200 Exclusive 2023",
+//         price: "$94000 | $960/month",
+//         id: 4,
+//         sold: "2023",
+//     },
+//     {
+//         image: "../asset/image/car5 glc300 4matic.jpg",
+//         name: "Mercedes GLC300 4Matic 2023",
+//         price: "$95000 | $950/month",
+//         id: 5,
+//         sold: "2023",
+//     },
+//     {
+//         image: "../asset/image/car6 maybach s680 4matic.jpg",
+//         name: "Mercedes-Maybach S680 4Matic 2023",
+//         price: "$600000 | $8000/month",
+//         id: 6,
+//         sold: "2023",
+//     },
+// ]
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-    {
-        Id: 5,
-        images: [],
-        name: "Mercedes GLC300 4Matic",
-        engine: "I4",
-        status: true,
-        sold:"12",
-        size: " 4670 x 1900 x 1650 (mm)",
-        wattage: "190 kW [258 hp] tại 6100 vòng/phút",
-        price: "$95000 | $950/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+// localStorage.setItem("products", JSON.stringify(products));
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-    {
-        Id: 6,
-        images: [],
-        name: "Mercedes-Maybach S680 4Matic",
-        engine: "V12",
-        status: true,
-        sold:"3",
-        size: "5469 x 1920 x1508 (mm)",
-        wattage: "450 kW [612 hp] tại 5500 vòng/phút",
-        price: "$600000 | $8000/month",
-        description: `Hưởng chương trình khuyến mãi của Hãng.
+// Lấy dữ liệu về đi render
+let products = JSON.parse(localStorage.getItem("products"));
 
-        Tặng phụ kiện khi mua xe
-        
-        Hỗ trợ mua xe trả góp, vay tới 80% giá trị xe, lãi suất vay ưu đãi nhất!
-        
-        Nhận giá tốt nhất & các chương trình khuyến mại.`
-    },
-];
-localStorage.setItem("producList", JSON.stringify(productList));
+
+
+function renderProduct(){
+    let element = "";
+    for(let i=0;i < products.length;i++ ){
+        element += 
+        `<div class="box">
+        <div class="box-img">
+          <img
+            src="${products[i].image}"
+            alt=""
+          />
+        </div>
+        <p>${products[i].sold}</p>
+        <h3>${products[i].name}</h3>
+        <h2>${products[i].price}</h2>
+        <a href="#" class="btn">Mua </a>
+        <br />
+        <a href="#" class="btn">Thêm vào giỏ</a>
+      </div>`
+      
+    }
+    document.getElementById("products").innerHTML=element;
+}
+renderProduct();
+
+// Function đi mua hàng
+function addToCart(){
+    // Đăng nhập mới cho đi mua hàng
+
+    let checkLogin = JSON.parse(localStorage.getItem("checkLogin"));
+    if(checkLogin==null){
+        alert("Bạn phải đăng nhập để đi mua hàng");
+        return;
+    }
+    console.log("đi mua hàng bthg");
+    // Lấy giở hàng để đi mua
+    let users = JSON.parse(localStorage.getItem("users"));
+    for(let i=0;i<users.length;i++){
+        if(users[i].id==checkLogin){
+            console.log("Giỏ hàng của bạn sẽ là: ", users[i].cart);
+            let product = JSON.parse(localStorage.getItem("products"));
+            for(let i=0;i<product.length;i++){
+                if(productId ==product[i].id){
+                    console.log("111",product[i]);
+                }
+            }  
+            break;
+        }
+    }
+}
--- JS/login.js
+++ JS/login.js
@@ -1,29 +1,33 @@
-//Elements của trang
+// Element của trang
 const formLogin = document.getElementById("formLogin");
-const emailElement =document.getElementById("email");
-const passwordElement =document.getElementById("password");
+const emailElement = document.getElementById("email");
+const passwordElement = document.getElementById("password");
 
-//Lắng nghe sự kiện
-formLogin.addEventListener("submit", function(e){
-    //Ngăn chặn sự kiện load
+
+// lắng nghe sự kiện submit form đăng nhập tài khoản
+formLogin.addEventListener("submit", function (e){
+    // Ngăn chặn sự kiện load lại trang
     e.preventDefault();
 
-    //Validate dữ liệu đầu vào
+    // Validate dữ liệu đầu vào
 
-    // Lấy dữ liệu từ local về
-    const userLocal =JSON.parse(localStorage.getItem("users")) || [];
+    //  Lấy dữ liệu từ local về
+    let userLocal = JSON.parse(localStorage.getItem("users")) || [];
 
-    
-    //Tìm kiếm email và mật khẩu ng dùng có trùng kh
-    const findUser = userLocal.find((user) => user.email === emailElement.value && user.password === passwordElement.value);
+    // Tìm kiếm email và mật khẩu người dùng nhập có tồn tại trên local?
+    const findUser = userLocal.find((user) => user.email === emailElement.value &&
+      user.password === passwordElement.value
+      );
 
     if(!findUser){
-        alert("Email hoặc mật khẩu không đúng")
+        alert("Email hoặc mật khẩu không đúng");
+    }else{
+        // Nếu có thì đăng nhập và chuyển hướng về trang chủ
+        
+        window.location.href = "index.html";
     }
-    else{
-        window.location.href = "indexUser.html";
-    }
-    //Nếu có thì đăng nhập
 
-    // Nếu ko thì sai
-});
+
+    // Lưu thông tin của user đăng nhập lên local
+    localStorage.setItem("userLogin", JSON.stringify(findUser));
+})
